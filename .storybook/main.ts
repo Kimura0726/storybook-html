2384ba4030e77e386992c8c41fbb24fc5420ac73
kimura0726 <0726.kimura@gmail.com>
2025-04-27 21:21:01 +0900

    initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/html-vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/html-vite",
+    options: {},
+  },
+  async viteFinal(config) {
+    // HTMLファイルをアセットとして扱う設定を追加
+    config.assetsInclude = [/\.html$/];
+
+    // または配列がすでに存在する場合は追加する
+    // if (Array.isArray(config.assetsInclude)) {
+    //   config.assetsInclude.push(/\.html$/);
+    // } else {
+    //   config.assetsInclude = [config.assetsInclude, /\.html$/].filter(Boolean);
+    // }
+
+    return config;
+  },
+};
+export default config;
---
+++ stories/h1.stories.ts
@@ -0,0 +1,4 @@
+import html from "./index.html?raw";
+
+export default { title: "index" };
+export const Preview = () => html;
